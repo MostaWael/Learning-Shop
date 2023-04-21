12d3325647875f68d5607e3c520e938dca262100
yasser-mohamed1 <y4ssermoh4med@gmail.com>
2023-04-21 03:06:28 +0200

    modified file

--- Not Academic/JS/main.js
+++ Not Academic/JS/main.js
@@ -1,28 +1,27 @@
 document.querySelector(".icon_container").onclick = function darkMode() {
   let icon = document.querySelector(".icon-dark");
   let isDark = icon.getAttribute("data-dark");
-  icon.classList.add('animated');
+  icon.classList.add("animated");
   if (isDark == "false") {
     this.classList.add("moon_mode");
     icon.classList.remove("fa-sun");
     icon.classList.add("fa-moon");
     icon.style.color = "#fff";
     this.style.backgroundColor = "#000";
-    document.querySelector(".lightMode").style.backgroundColor = "#242424"
+    document.querySelector(".lightMode").style.backgroundColor = "#242424";
     document.querySelector("body").style.backgroundColor = "var(--black-color)";
-  }
-  else {
+  } else {
     this.classList.remove("moon_mode");
     icon.classList.remove("fa-moon");
     icon.classList.add("fa-sun");
     icon.style.color = "#FFDE59";
     this.style.backgroundColor = "#fff";
-    document.querySelector(".lightMode").style.backgroundColor = "#ebebeb"
+    document.querySelector(".lightMode").style.backgroundColor = "#ebebeb";
     document.querySelector("body").style.backgroundColor = "var(--white-color)";
   }
-  icon.setAttribute("data-dark", (isDark == "true" ? "false" : "true"));
+  icon.setAttribute("data-dark", isDark == "true" ? "false" : "true");
   console.log(isDark);
-  setTimeout( () => {
-    icon.classList.remove('animated');
-  }, 500)
-}
+  setTimeout(() => {
+    icon.classList.remove("animated");
+  }, 500);
+};
