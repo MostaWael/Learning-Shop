903ab463d8b212d8c1edab08f0f112395499118b
lfg6f4w <abdalrazekmohmed6@gmail.com>
2023-04-24 23:13:00 +0200

    modified file

--- Not Academic/JS/main.js
+++ Not Academic/JS/main.js
@@ -1,3 +1,4 @@
+let root = document.querySelector(':root');
 document.querySelector(".icon_container").onclick = function darkMode() {
   let icon = document.querySelector(".icon-dark");
   let isDark = icon.getAttribute("data-dark");
@@ -9,7 +10,10 @@ document.querySelector(".icon_container").onclick = function darkMode() {
     icon.style.color = "#fff";
     this.style.backgroundColor = "#000";
     document.querySelector(".lightMode").style.backgroundColor = "#242424";
-    document.querySelector("body").style.backgroundColor = "var(--black-color)";
+    root.style.setProperty('--black-color', '#000');
+    root.style.setProperty('--white-color', '#fff');
+    root.style.setProperty('--black-light-color', '#242424');
+    root.style.setProperty('--textWhite50', '#a6a3a3d4');
   } else {
     this.classList.remove("moon_mode");
     icon.classList.remove("fa-moon");
@@ -17,7 +21,10 @@ document.querySelector(".icon_container").onclick = function darkMode() {
     icon.style.color = "#FFDE59";
     this.style.backgroundColor = "#fff";
     document.querySelector(".lightMode").style.backgroundColor = "#ebebeb";
-    document.querySelector("body").style.backgroundColor = "var(--white-color)";
+    root.style.setProperty('--black-color', '#fff');
+    root.style.setProperty('--white-color', '#000');
+    root.style.setProperty('--black-light-color', '#ddd');
+    root.style.setProperty('--textWhite50', '#2d2d2dd1');
   }
   icon.setAttribute("data-dark", isDark == "true" ? "false" : "true");
   console.log(isDark);
